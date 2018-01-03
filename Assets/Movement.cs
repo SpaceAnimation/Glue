65bdb265237f420059b69a6686b833af291efba5
Spacey <agphysics@live.com>
2018-01-03 15:25:49 +1000

    replced all files within branch
    changed master instead of Spacey branch

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -7,6 +7,8 @@ public class Movement : MonoBehaviour {
 	public GameObject player;
 	public Rigidbody energySource;
 	public Rigidbody driver;
+	public float x;
+	public float z;
 
 	// Use this for initialization
 	void Start () {
@@ -15,7 +17,11 @@ public class Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		x = Input.GetAxis ("Horizontal") * Time.deltaTime * 150.0f;
+		z = Input.GetAxis ("Vertical") * Time.deltaTime * 3.0f;
+
+		energySource.gameObject.transform.Rotate (0, x, 0);
+		player.gameObject.transform.Translate (0, 0, z);
 	}
 
 	void FixedUpdate()
