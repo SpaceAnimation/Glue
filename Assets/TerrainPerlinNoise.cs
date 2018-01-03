dc0f752856a5eec3ac2600231c87c8b7c3e63659
David Hawker <davidhawkeris@gmail.com>
2018-01-03 19:13:07 +1000

    terrain

---
+++ Assets/TerrainPerlinNoise.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainPerlinNoise : MonoBehaviour {
+
+    public int width = 100;
+    public int height = 100;
+    public int depth = 10;
+    public float scale = 10.0f;
+    public float offsetX = 100f;
+    public float offsetY = 100f;
+
+    // Use this for initialization
+    void Start () {
+        offsetX = Random.Range(0, 9999f);
+        offsetY = Random.Range(0, 9999f);
+
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+    }
+
+    TerrainData GenerateTerrain(TerrainData terrainData)
+    {
+        terrainData.heightmapResolution = width + 1;
+        terrainData.size = new Vector3(width, depth, height);
+        terrainData.SetHeights(0, 0, GenerateHeights());
+
+        float[,,] map = new float[terrainData.alphamapWidth, terrainData.alphamapHeight, terrainData.alphamapLayers];
+
+        // For each point on the alphamap...
+        for (int y = 0; y < terrainData.alphamapHeight; y++)
+        {
+            for (int x = 0; x < terrainData.alphamapWidth; x++)
+            {
+                // Get the normalized terrain coordinate that
+                // corresponds to the the point.
+                float normX = (float)x * 1.0f / ((float)terrainData.alphamapWidth - 1);
+                float normY = (float)y * 1.0f / ((float)terrainData.alphamapHeight - 1);
+
+                // Get the steepness value at the normalized coordinate.
+                float angle = terrainData.GetSteepness(normX, normY);
+
+                // Steepness is given as an angle, 0..90 degrees. Divide
+                // by 90 to get an alpha blending value in the range 0..1.
+                float frac = angle / 90.0f;
+                map[x, y, 0] = frac;
+                map[x, y, 1] = 1 - frac;
+            }
+        }
+
+        terrainData.SetAlphamaps(0, 0, map);
+
+        return terrainData;
+    }
+
+    float[,] GenerateHeights()
+    {
+        float[,] heights = new float[width, height];
+
+        for(int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                heights[x, y] = CalculateHeight(x, y);
+            }
+        }
+
+        return heights;
+    }
+
+    float CalculateHeight(int x, int y)
+    {
+        float xCoord = (float)x / width * scale + offsetX;
+        float yCoord = (float)y / height * scale + offsetY;
+
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+}
---
+++ Assets/TextureCreator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureCreator : MonoBehaviour {
+
+    public int resolution = 256;
+
+    private Texture2D texture;
+
+    private void Awake()
+    {
+        texture = new Texture2D(resolution, resolution, TextureFormat.RGB24, true);
+        texture.name = "Procedural Texture";
+        GetComponent<MeshRenderer>().material.mainTexture = texture;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
