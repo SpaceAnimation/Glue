6655205e7fa56d0fca8dd2b1d4e5a65b9049c6b3
Spacey <agphysics@live.com>
2018-01-17 22:32:37 +1000

    changed movement
    camera follows player and has smooth

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public Vector3 velocity;
+	public float smoothSpeed = 0.125f;
+	public Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 desieredPos = target.position + offset;
+		Vector3 smoothPos = Vector3.Lerp(transform.position, desieredPos, smoothSpeed);
+		transform.position = smoothPos;
+
+		transform.LookAt (target);
+	}
+}
--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -8,21 +8,25 @@ public class Movement : MonoBehaviour {
 	public GameObject player;
 	public Rigidbody energySource;
 	public Rigidbody driver;
+	public Vector3 velocity;
+	public float MAXSPEED;
 	public float x;
-	public float z;
+	public Vector3 z;
 
 	// Use this for initialization
 	void Start () {
-
+		velocity = Vector3.zero;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		x = Input.GetAxis ("Horizontal") * Time.deltaTime * 150.0f;
-		z = Input.GetAxis ("Vertical") * Time.deltaTime * 3.0f;
+		z.z = Input.GetAxis ("Vertical") * Time.deltaTime;//* 3.0f;
+		if(z.z < MAXSPEED)
+			velocity += z;
 
 		energySource.gameObject.transform.Rotate (0, x, 0);
-		energySource.gameObject.transform.Translate (0, 0, z);
+		energySource.gameObject.transform.Translate (velocity);
 	}
 
 	void FixedUpdate()
